8b4b5c3c27207bdbf4aba60dd0b5ae148220b6f0
k1921 <k1921@student.jamk.fi>
2020-05-26 16:42:27 +0300

    gitignore fix

--- 2/maidentiedot/src/components/DisplayResults.js
+++ 2/maidentiedot/src/components/DisplayResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Buttons from "./Buttons";
+import Weather from "./Weather";
 
 const DisplayResults = (props) => {
   const filter = props.filter;
@@ -12,6 +13,23 @@ const DisplayResults = (props) => {
         <></>
       ) : filtered.length > 10 ? (
         <p>Too many matches, spesicfy your search</p>
+      ) : filtered.length === 1 ? (
+        <>
+          <p />
+          <h2>{filtered[0].name}</h2>
+          <h3>Capital </h3>
+          <p>{filtered[0].capital}</p>
+          <h3>Population </h3>
+          <p>{filtered[0].population}</p>
+          <h3>Languages </h3>
+          <ul>
+            {filtered[0].languages.map((lang) => (
+              <li>{lang.name}</li>
+            ))}
+          </ul>
+          <img src={filtered[0].flag} height={150} />
+          <Weather country={filtered[0]} />
+        </>
       ) : (
         <Buttons countries={filtered} />
       )}
---
+++ app/src/components/Notification.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return <div className="error">{message}</div>;
+};
+export default Notification;
