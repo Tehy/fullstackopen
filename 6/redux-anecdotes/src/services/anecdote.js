45cb4d6621e6477a66b1bec7cfb3442cf05a7c40
k1921 <k1921@student.jamk.fi>
2020-10-15 18:30:50 +0300

    services update

--- 6/redux-anecdotes/src/services/anecdote.js
+++ 6/redux-anecdotes/src/services/anecdote.js
@@ -1,25 +1,20 @@
 import axios from "axios";
-
 const baseUrl = "http://localhost:3001/anecdotes";
 
 const getSingle = async (id) => {
-  return getResponseBody(axios.get(`${baseUrl}/${id}`));
+  return getResponseData(axios.get(`${baseUrl}/${id}`));
 };
-
 const getAll = async () => {
-  return getResponseBody(axios.get(baseUrl));
+  return getResponseData(axios.get(baseUrl));
 };
-
 const create = async (anecdote) => {
-  return getResponseBody(axios.post(baseUrl, anecdote));
+  return getResponseData(axios.post(baseUrl, anecdote));
 };
-
 const update = async (anecdote) => {
-  return getResponseBody(axios.put(`${baseUrl}/${anecdote.id}`, anecdote));
+  return getResponseData(axios.put(`${baseUrl}/${anecdote.id}`, anecdote));
 };
-
-const getResponseBody = async (operation) => {
-  const response = await operation;
+const getResponseData = async (response) => {
+  const response = await response;
   return response.data;
 };
 
